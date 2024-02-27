c3f74f53c1c2f01924acf4baf8ed997597cd4b1e
gabriel <nobrecoder@gmail.com>
2024-02-27 12:09:33 -0300

    27/02/2024

--- src/br/com/nobrecoder/model/Memory.java
+++ src/br/com/nobrecoder/model/Memory.java
@@ -11,12 +11,15 @@ import java.util.List;
  * */
 public class Memory {
 	
-	private enum TypeComand { //Essa enum foi criada de forma embutida dentro da classe Memory. A missão dela é identificar que tipo de botão foi clicado na calculadora, através da identificação do tipo de botão, é possível fazer operações de forma correta.
+	private enum TypeCommand { //Essa enum foi criada de forma embutida dentro da classe Memory. A missão dela é identificar que tipo de botão foi clicado na calculadora, através da identificação do tipo de botão, é possível fazer operações de forma correta.
 		TO_ZERO, NUMBER, DIV, MULT, SUB, SUM, EQUAL, COMMA
 	}
 	private static final Memory INSTANCE_OF_MEMORY = new Memory(); //Aqui temos o objeto estático único da classe, por ser privado, só poderá ser chamado de forma estática.
 	private final List<ObserverMemory> observers = new ArrayList<>(); //Nesta lista serão armazenados todos os observers interessados em saber quando um botão foi clicado e que tipo de botão foi clicado.
 	private String atualText = ""; //Note que o texto atual por ser dinamico poderá ser chamado por através de um getter a partir da instancia de Memory.
+	private String bufferText = "";
+	private boolean replaceDisplay = false;
+	private TypeCommand lastOperator = null; 
 	
 	private Memory() {} //Constructor é privado, isso que faz o padrão Singleton acontecer, pois o constructor nunca poderá ser chamado fora da classe.
 	
@@ -40,17 +43,56 @@ public class Memory {
 	 * */
 	public void processCalc(String value) { //Recebe uma string como valor, essa string contém a string que fica no botão.
 		
-		TypeComand typeComand = detectComand(value); //Recebe o tipo de botão, que é identificado pela enum acima, por através do método "detectComand", que faz um filtro dos valores passados para identificar qual é o tipo de botão correto.
-		System.out.println(typeComand);
+		TypeCommand typeCommand = detectComand(value); //Recebe o tipo de botão, que é identificado pela enum acima, por através do método "detectComand", que faz um filtro dos valores passados para identificar qual é o tipo de botão correto.
 		
-		atualText += value; //Adiciona o valor passado ao valor pré-existente em memória.
+		if(typeCommand == null) {
+			return;
+		} else if(typeCommand == TypeCommand.TO_ZERO) {
+			atualText = "";
+			bufferText = "";
+			replaceDisplay = false;
+			lastOperator = null;
+		} else if(typeCommand == TypeCommand.NUMBER || typeCommand == TypeCommand.COMMA) {
+			atualText = replaceDisplay ? value : atualText + value;
+			replaceDisplay = false;
+		} else {
+			replaceDisplay = true;
+			atualText = getResultOfOperation();
+			bufferText = atualText;
+			lastOperator = typeCommand;
+		}
+		
 		observers.forEach(o -> o.alteredValue(getAtualText())); //Avisa todos os observadores que o botão foi clicado e o valor do display deverá ser alterado.
+	}
+
+	private String getResultOfOperation() {
+		if(lastOperator == null) {
+			return atualText;
+		}
+		
+		double bufferNumber = Double.parseDouble(bufferText.replace(",", "."));
+		double atualNumber = Double.parseDouble(atualText.replace(",", "."));
+		double result = 0;
+		
+		if(lastOperator == TypeCommand.SUM) {
+			result = bufferNumber + atualNumber;
+		} else if(lastOperator == TypeCommand.SUB) {
+			result = bufferNumber - atualNumber;
+		} else if(lastOperator == TypeCommand.MULT) {
+			result = bufferNumber * atualNumber;
+		} else if(lastOperator == TypeCommand.DIV) {
+			result = bufferNumber / atualNumber;
+		}
+		
+		String stringResult = Double.toString(result).replace(".", ",");
+		boolean integer = stringResult.endsWith(",0");
+		return integer ? stringResult.replace(",0", "") : stringResult;
 	}
 
 	/**@Objetivo
 	 * Identificar o tipo de botão que foi clicado por através das enums...
 	 * */
-	private TypeComand detectComand(String value) { //Recebe como parâmetro o texto que está escrito encima do botão.
+	private TypeCommand detectComand(String value) { //Recebe como parâmetro o texto que está escrito encima do botão.
 
 		if(atualText.isEmpty() && value == "0") { //Esse laço "if" estabelece a lógica para não fazer nada quando uma calculadora já estiver com o valor zero e um zero for clicado. Tornando o display inalterado.
 			return null;
@@ -58,22 +100,22 @@ public class Memory {
 		
 		try { //O try catch identifica se o valor passado pode ser convertido em um número ou não, se puder, ele retorna a informação de que o botão clicado é do tipo NUMBER... 
 			Integer.parseInt(value);
-			return TypeComand.NUMBER;
+			return TypeCommand.NUMBER;
 		} catch (NumberFormatException e) { //Se o valor não for um NUMBER, entra-se no catch...
 			if("AC".equals(value)){ //Esse laço "if" identifica cada operação da calculadora retornando a sua enum correspondente...
-				return TypeComand.TO_ZERO;
+				return TypeCommand.TO_ZERO;
 			} else if("/".equals(value)) {
-				return TypeComand.DIV;
+				return TypeCommand.DIV;
 			} else if("*".equals(value)) {
-				return TypeComand.MULT;
+				return TypeCommand.MULT;
 			} else if("+".equals(value)) {
-				return TypeComand.SUM;
+				return TypeCommand.SUM;
 			} else if("-".equals(value)) {
-				return TypeComand.SUB;
+				return TypeCommand.SUB;
 			} else if("=".equals(value)) {
-				return TypeComand.EQUAL;
-			} else if(",".equals(value)) {
-				return TypeComand.COMMA;
+				return TypeCommand.EQUAL;
+			} else if(",".equals(value) && !atualText.contains(",")) {
+				return TypeCommand.COMMA;
 			}
 		}
 		
